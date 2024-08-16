e73e30b658609bed60ec662d08fe32d804cfbef0
Suvasish Sahoo <suvasish94@gmail.com>
2024-08-16 20:52:37 +0530

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    //
+    function addProduct(Request $req){
+        $product = new Product;
+        $product->name = $req->input("name");
+        $product->price = $req->input("price");
+        $product->description = $req->input("description");
+        $product->file_path= $req->file("file")->store("products");
+        $product->save();
+        return $product;
+    }
+
+    function list(){
+        return Product::all();
+    }
+
+    function delete($id){
+        $result = Product::where("id",$id)->delete();
+        if($result){
+            return ['result'=>"Product has been deleted"];
+        }
+        else{
+            return ["result"=> "Operation failed"];
+        }
+    }
+
+    function getProduct($id){
+        return Product::find( $id );
+    }
+
+
+    // function store(){
+    //     return null;
+    function updateProduct($id, Request $req){
+        // return $req->input();
+        $product = Product::find($id);
+        $product->name = $req->input("name");
+        $product->price = $req->input("price");
+        $product->description = $req->input("description");
+        if($req-> file('file')){
+            $product->file_path= $req->file("file")->store("products");
+        }
+        $product->save();
+        return $product;
+    }
+
+    function searchProduct($key){
+        return Product::where('name','Like',"%$key%")->get();
+    }
+
+}
